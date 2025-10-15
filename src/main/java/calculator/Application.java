98007872b41639fdce6990d03523276188988d2a
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-10-15 13:24:09 +0900

    feat(calculator) : 커스텀 구분자 기능
    기존 계산기는 쉼표(,)와 콜론(:)으로 고정된 기본 구분자만 지원했으나, 사용자가 직접 구분자를 지정할 수 있는 커스텀 구분자 기능을 추가하여 완성하였습니다.
    
    이제 `//;\n1;2;3`과 같은 입력을 이제 올바르게 계산할 수 있습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,12 +4,38 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Pattern;
+import java.util.Scanner;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     private static final Set<String> seperator = new HashSet<>(Arrays.asList(",", ":"));
-    private static final String regex = "//[a-zA-Z\\W_]\\\\n";
     private static int sum = 0;
+    private static final String regex = "//[a-zA-Z\\W_]\\\\n";
+    private static final Scanner sc = new Scanner(System.in);
+
+    /**
+     * 커스텀 구분자가 있는지 검사하는 메소드 입니다.
+     * 커스텀 구분자를 찾아내면 계산식만 있는 형태로 반환합니다.
+     *
+     * @param input 입력 문자열
+     * @return 계산식 문자열
+     */
+    private static String checkCustomSeperator(String input) {
+        String expression = "";
+        if(input.length() > 4) {
+            // 문자열 앞 5글자가 커스텀 구분자 정의라면 구분자를 추가하고 실제 표현식 위치로 지정
+            if(Pattern.matches(regex, input.substring(0, 5))) {
+                seperator.add(input.substring(2, 3));
+                expression = input.substring(5);
+            } else { // 아닐경우는 그냥 그대로 시작
+                expression = input;
+            }
+        } else { // 길이가 충족이 안되면 정의가 안되어있으므로
+            expression = input;
+        }
+
+        return expression;
+    }
 
     /**
      * 계산식 문자열을 받아 숫자로 변환하고 합산합니다.
@@ -54,11 +80,13 @@ public class Application {
         // 1. 입력
         String input = Console.readLine(); // 입력
 
-        // 2. 계산
-        calculate(input);
+        // 2. 커스텀 구분자 존재 확인
+        String expression = checkCustomSeperator(input); // 실제로 계산에 쓰일 표현식
 
-        // 3. 결과 출력
+        // 3. 계산
+        calculate(expression);
+
+        // 4. 결과 출력
         System.out.println("결과 : " + sum);
-
     }
 }
